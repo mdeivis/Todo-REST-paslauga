a7ad558fef6b48436ae2764d7d1c208c65db3d70
mdeivis94 <m.deividas94@gmail.com>
2017-03-13 23:27:25 +0200

    Added comment

--- todo/toodAppMiddleware.js
+++ todo/toodAppMiddleware.js
@@ -1,6 +1,7 @@
 const httpStatuses = require('http-codes');
 
 const dataValidation = (req, res, next) => {
+    // Here we can create function for each rule. 
     if (!req.body) {
         res.status(httpStatuses.BAD_REQUEST).send('Data is required');
     } else {
@@ -9,8 +10,6 @@ const dataValidation = (req, res, next) => {
         } else if (req.body.name.length > 100) {
             res.status(httpStatuses.BAD_REQUEST).send('Name is to long. Max length 100 characters');
         }
-        
-        console.log(req.body.name.length)
     }
     
     next();
